b451acee16669b04c9da57ba29879c29160cdbf6
rafelitohmendez-ux <rafelitohmendez@gmail.com>
2026-01-08 23:07:24 -0500

    Update page.jsx

--- app/rag/page.jsx
+++ app/rag/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-
+export const dynamic = 'force-dynamic';
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
