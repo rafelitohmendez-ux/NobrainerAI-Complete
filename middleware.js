0028ef4f7f064da94861d78c90a3ca77c5783918
rafelitohmendez-ux <rafelitohmendez@gmail.com>
2026-01-10 15:55:02 -0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -3,8 +3,12 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 
 const isProtectedRoute = createRouteMatcher([
-    '/dashboard(.*)','/tool(.*)','/upload(.*)','/podcast(.*)',
-  ]);
+    '/dashboard(.*)',
+    '/tool(.*)',
+    '/upload(.*)',
+    '/podcast(.*)',
+    '/pricing(.*)', // Add this line
+]);
 
   export default clerkMiddleware((auth, req) => {
     if (isProtectedRoute(req)) auth().protect();
